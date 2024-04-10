05b913ac6bf28b3a91490203e1bf8341f5c5f752
Kevin Chen <84763316+kevinjxc@users.noreply.github.com>
2024-04-09 20:25:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+//Hamburger Variables Created
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+//Event Listener for Hamburger Created
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
+
+//Lists Items in Hamburger
+document.querySelectorAll(".item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}))
+
+//Creates Variables to Count Slide Index
+let slideIndex = 0;
+showSlides();
+
+//Shows Slides on an Automated Timer
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000);
+}
+
+//Creates Variables to Count Slide Index
+let slideIndex2 = 0;
+showSlides2();
+
+//Shows Slides on an Automated Timer
+function showSlides2() {
+    let i1;
+    let slides = document.getElementsByClassName("mySlides2");
+    for (i1 = 0; i1 < slides.length; i1++) {
+      slides[i1].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2-1].style.display = "block";
+    setTimeout(showSlides2, 5100);
+}
+
+//Creates Variables to Count Slide Index
+let slideIndex3 = 0;
+showSlides3();
+
+//Shows Slides on an Automated Timer
+function showSlides3() {
+    let slides = document.getElementsByClassName("mySlides3");
+    for (i1 = 0; i1 < slides.length; i1++) {
+      slides[i1].style.display = "none";
+    }
+    slideIndex3++;
+    if (slideIndex3 > slides.length) {slideIndex3 = 1}
+    slides[slideIndex3-1].style.display = "block";
+    setTimeout(showSlides3, 5200);
+}
+
+//Redirects to About Page
+function pageAbout() {
+  window.location.href = "about.html";
+}
+
+//Redirects to Apply Page
+function pageApply() {
+  window.location.href = "apply.html";
+}
+
+//Redirects to STREAM Page
+function pageSTREAM() {
+  window.location.href = "stream.html";
+}
+
+//Redirects to Contact Page
+function pageContact() {
+  window.location.href = "contact.html";
+}
+
+//Redirects to Home Page
+function pageIndex() {
+  window.location.href = "index.html";
+}
+
+//Redirects to Email
+function pageEmail() {
+  window.location.href = "mailto:upstreamprogram@gmail.com";
+}
+
+//Redirects to Phone Number
+function pagePhone() {
+  window.location.href = "tel:+12064864950";
+}
+
+//Redirects to Seattle Map
+function pageLocation() {
+  window.open("https://www.google.com/maps/place/Seattle,+WA");
+}
